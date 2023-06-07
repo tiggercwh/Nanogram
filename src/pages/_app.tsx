1d3b17ed0c3fd22a906a7e1e958ddf9868c6cdfd
tiggercwh <whchanar@connect.ust.hk>
2023-06-08 04:28:13 +0800

    feat: add respones caching

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -51,23 +51,4 @@ export default withTRPC<AppRouter>({
    * @link https://trpc.io/docs/ssr
    */
   ssr: false,
-  responseMeta(opts) {
-    const { ctx, paths, clientErrors, type } = opts;
-    // assuming you have all your public routes with the keyword `public` in them
-    const allPublic = paths && paths.every((path) => path.includes("public"));
-    // checking that no procedures errored
-    const allOk = clientErrors.length === 0;
-    // checking we're doing a query request
-    const isQuery = type === "query";
-    if (ctx?.res && allPublic && allOk && isQuery) {
-      // cache request for 1 day + revalidate once every second
-      const ONE_DAY_IN_SECONDS = 60 * 60 * 24;
-      return {
-        headers: {
-          "cache-control": `s-maxage=1, stale-while-revalidate=${ONE_DAY_IN_SECONDS}`,
-        },
-      };
-    }
-    return {};
-  },
 })(MyApp);
