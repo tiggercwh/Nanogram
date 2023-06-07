ee4fc320c39f634551a45e6a85fefb03dcdfda31
tiggercwh <whchanar@connect.ust.hk>
2023-06-08 04:35:14 +0800

    fix: change route checking logic

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -8,13 +8,12 @@ export default createNextApiHandler({
   router: appRouter,
   createContext: createContext,
   responseMeta({ ctx, paths, type, errors }) {
-    // assuming you have all your public routes with the keyword `public` in them
-    const allPublic = paths && paths.every((path) => path.includes("public"));
+    console.log(ctx.res, { paths, type, errors });
     // checking that no procedures errored
     const allOk = errors.length === 0;
     // checking we're doing a query request
     const isQuery = type === "query";
-    if (ctx?.res && allPublic && allOk && isQuery) {
+    if (ctx?.res && allOk && isQuery) {
       // cache request for 1 day + revalidate once every second
       const ONE_DAY_IN_SECONDS = 60 * 60 * 24;
       return {
