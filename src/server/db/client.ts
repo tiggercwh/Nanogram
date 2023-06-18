591587107d8e8690b1f416a4a9dd558618dc5b1c
tiggercwh <whchanar@connect.ust.hk>
2023-06-18 19:02:23 +0800

    feat: add vercel kv

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -1,5 +1,17 @@
 // src/server/db/client.ts
 import { PrismaClient } from "@prisma/client";
+import { createPrismaRedisCache } from "prisma-redis-middleware";
+import Redis from "ioredis";
+import type { Prisma as PrismaType } from "@prisma/client";
+
+const { KV_URL } = process.env;
+const redis = new Redis(KV_URL!, {
+  maxRetriesPerRequest: 1,
+  connectTimeout: 10000,
+  tls: {
+    rejectUnauthorized: true,
+  },
+});
 
 declare global {
   var prisma: PrismaClient | undefined;
@@ -10,6 +22,30 @@ export const prisma =
   new PrismaClient({
     log: ["query"],
   });
+
+const cacheMiddleware: PrismaType.Middleware = createPrismaRedisCache({
+  models: [{ model: "Level" }],
+  storage: {
+    type: "redis",
+    options: {
+      client: redis,
+      invalidation: { referencesTTL: 300 },
+      log: console,
+    },
+  },
+  cacheTime: 3600,
+  onHit: (key) => {
+    console.log("hit", key);
+  },
+  onMiss: (key) => {
+    console.log("miss", key);
+  },
+  onError: (key) => {
+    console.log("error", key);
+  },
+});
+
+prisma.$use(cacheMiddleware);
 
 if (process.env.NODE_ENV !== "production") {
   global.prisma = prisma;
